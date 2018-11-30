398661053f7a8acfac2abee485f341b338fab91c
Robert Geirhos <rgeirhos@web.de>
2018-11-30 14:47:29 +0100

    Added 2 other models.

--- models/load_pretrained_models.py
+++ models/load_pretrained_models.py
@@ -10,11 +10,12 @@ import torchvision.models
 from torch.utils import model_zoo
 
 
-
 def load_model(model_name):
 
     model_urls = {
-            'resnet50': 'https://bitbucket.org/robert_geirhos/texture-vs-shape-pretrained-models/raw/6f41d2e86fc60566f78de64ecff35cc61eb6436f/resnet50_train_60_epochs-c8e5653e.pth.tar',
+            'resnet50_trained_on_SIN': 'https://bitbucket.org/robert_geirhos/texture-vs-shape-pretrained-models/raw/6f41d2e86fc60566f78de64ecff35cc61eb6436f/resnet50_train_60_epochs-c8e5653e.pth.tar',
+            'resnet50_trained_on_SIN_and_IN': 'https://bitbucket.org/robert_geirhos/texture-vs-shape-pretrained-models/raw/60b770e128fffcbd8562a3ab3546c1a735432d03/resnet50_train_45_epochs_combined_IN_SF-2a0d100e.pth.tar',
+            'resnet50_trained_on_SIN_and_IN_then_finetuned_on_IN': 'https://bitbucket.org/robert_geirhos/texture-vs-shape-pretrained-models/raw/60b770e128fffcbd8562a3ab3546c1a735432d03/resnet50_finetune_60_epochs_lr_decay_after_30_start_resnet50_train_45_epochs_combined_IN_SF-ca06340c.pth.tar',
     }
 
     model = torchvision.models.resnet50(pretrained=False)
@@ -26,9 +27,14 @@ def load_model(model_name):
 
 if __name__ == "__main__":
 
-    model_name = "resnet50" # ResNet-50 trained for 60 epochs on Stylized-ImageNet (SIN).
-    model = load_model(model_name)
+    # Abbreviations:
+    # SIN = Stylized-ImageNet
+    #  IN = normal, standard ImageNet
 
+    model_A = "resnet50_trained_on_SIN"
+    model_B = "resnet50_trained_on_SIN_and_IN"
+    model_C = "resnet50_trained_on_SIN_and_IN_then_finetuned_on_IN"
+    model = load_model(model_A) # change to different model as desired
  
     for k, v in model.module.state_dict().items():
         print(k)
